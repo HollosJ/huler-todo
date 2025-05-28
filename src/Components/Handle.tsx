0fb2d18fae9ef7471dc1453e65a0e60931bd0796
HollosJ <hollosj99@gmail.com>
2025-05-28 15:32:24 +0100

    Grab handle styling tweaks

--- src/Components/Handle.tsx
+++ src/Components/Handle.tsx
@@ -4,14 +4,17 @@ const Handle = styled.button`
   background: none;
   border: none;
   -webkit-appearance: none;
-  margin-right: 24px;
-  cursor: grabbing;
-  width: 10px;
-  height: 16px;
+  cursor: grab;
+  width: 17px;
+  height: 28px;
   background-image: url("Handle.svg");
   background-size: contain;
   background-repeat: no-repeat;
   background-position: center;
+
+  &:active {
+    cursor: grabbing;
+  }
 `;
 
 export default Handle;
