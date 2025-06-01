f077b5ce87ee5d1ec846d7dc01f7d0ab2d039d60
HollosJ <hollosj99@gmail.com>
2025-06-01 20:28:33 +0100

    Add todo form styling fixes

--- src/Components/AddTodo.tsx
+++ src/Components/AddTodo.tsx
@@ -34,53 +34,51 @@ const AddTodo = ({ className, onSubmit }: AddTodoProps) => {
 };
 
 const StyledAddTodo = styled(AddTodo)`
-  margin-top: 63px;
-  margin-bottom: 75px;
-  z-index: ${(props) => props.theme.zLayers.overlay};
-  overflow: hidden;
+  margin: 70px auto;
   display: flex;
   gap: 26px;
   width: 100%;
   font-weight: 300;
 
   input {
-    padding: 20px 50px;
+    padding: 20px;
+    width: 100%;
     height: 70px;
     background-color: ${(props) => props.theme.colors.grey2};
+    color: ${(props) => props.theme.colors.text};
     border: 2px solid ${(props) => props.theme.colors.grey1};
     border-radius: 999px;
-    outline: none;
-    color: ${(props) => props.theme.colors.text};
     font-family: ${(props) => props.theme.fonts.primary};
     font-size: 20px;
-    flex: 1;
-    &::placeholder {
-      color: ${(props) => props.theme.colors.text};
-      font-weight: 300;
-    }
+  }
 
-    &:focus-visible {
-      border: 2px solid ${(props) => props.theme.colors.grey3};
-    }
+  input:focus-visible {
+    border: 2px solid ${(props) => props.theme.colors.grey3};
   }
 
   button {
     width: 70px;
     height: 70px;
-    border-radius: 999px;
-    border: none;
-    background-color: ${(props) => props.theme.colors.primary};
+    border-radius: 50%;
+    background: ${(props) => props.theme.colors.primary};
+    flex-shrink: 0;
     display: flex;
     align-items: center;
     justify-content: center;
-    cursor: pointer;
-    &:before {
-      content: "";
-      background-image: url("Plus.svg");
-      background-size: contain;
-      background-repeat: no-repeat;
-      width: 32px;
-      height: 32px;
+  }
+
+  button:before {
+    content: "";
+    background-image: url("Plus.svg");
+    background-size: contain;
+    background-repeat: no-repeat;
+    width: 32px;
+    height: 32px;
+  }
+
+  @media (min-width: ${(props) => props.theme.breakpoints.small}) {
+    input {
+      padding: 20px 50px;
     }
   }
 `;
